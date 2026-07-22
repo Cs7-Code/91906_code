bfede21647d144b90d9d8d74cd5e654e4768d55d
Cs7-Code <timothydastracey@gmail.com>
2026-07-22 14:52:49 +1200

    Updated Login GUI
    Updated Login Page, with added functionality for password/pin entry by enabling the hiding of passwords for security

--- src/main.py
+++ src/main.py
@@ -1,27 +1,54 @@
 import flet as ft
+import sqlite3 as sql
+
+def login(user_id, user_password):
+    conn = sql.connect("user.db")
+
+    curs = conn.cusor()
+
+
 
 
 def main(page: ft.Page):
+    #Configuring Page 
     page.title = "Login"
 
-    page.add(ft.Text("Login"))
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.CrossAxisAlignment.CENTER
+
+    #Page Title
+
+    login_title = ft.Text("Login", theme_style=ft.TextThemeStyle.DISPLAY_SMALL)
+
+    #User ID Fields 
 
     user_name_icon = ft.Icon(ft.Icons.ACCOUNT_CIRCLE_ROUNDED, color=ft.Colors.PRIMARY, size=40)
 
-    user_name_text_field = ft.TextField(label="User ID")
+    user_name_text_field = ft.TextField(label="User ID", max_length=5, counter="")
 
-    user_name_field = ft.Row([user_name_icon, user_name_text_field])
+    user_name_field = ft.Row([user_name_icon, user_name_text_field], tight=True)
 
-    page.add(user_name_field)
+    #Password fields
 
     user_password_icon = ft.Icon(ft.Icons.PASSWORD, color=ft.Colors.PRIMARY, size=40)
 
-    user_name_text_field = ft.TextField(label="User ID")
+    user_name_text_field = ft.TextField(label="Password/Pin", password=True, can_reveal_password=True)
 
-    user_password_field = ft.Row([user_password_icon, user_name_text_field])
+    user_password_field = ft.Row([user_password_icon, user_name_text_field], tight=True)
 
-    page.add(user_password_field)
+    #Login fields 
+
+    login_fields = ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER, 
+                             controls=[login_title, user_name_field, user_password_field])
+
     
+
+    page.add(ft.Card(shadow_color=ft.Colors.ON_SURFACE_VARIANT, 
+                     content=ft.Container(padding=10, content=login_fields)
+                    )
+            )
+
+    #Login button
 
     page.add(ft.FilledButton("Login"))
 
