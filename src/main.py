f887e1c9b41310e24dfc7482fa7e0bde64fa4008
Cs7-Code <timothydastracey@gmail.com>
2026-08-10 12:44:23 +1200

    Fixed and integrated login system
    Fixed and integrated login system, need to add redirect to home page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-# import flet as ft
-# from faker import Faker
-from typing import Dict
+import os
+
+import flet as ft
 from sqlalchemy.exc import IntegrityError
-from sqlmodel import Column, Field, SQLModel, create_engine, Session, select, JSON
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(primary_key=True)
-    name: Dict[str, str] = Field(sa_column=Column(JSON))
+    #name: dict[str, str] = Field(sa_column=Column(JSON))
     user_id: int = Field(ge=1000, le=9999, unique=True, nullable=False)
     user_pass: str = Field(nullable=False)
 
@@ -27,9 +27,9 @@ class User(SQLModel, table=True):
             except IntegrityError:
                 session.rollback()
                 return "User creation failed (User ID not unique)"
-            except Exception as e:
+            except Exception:
                 session.rollback()
-                return f"An error occured (Error Details: {e})"
+                return f"An error occured (Error Details: {Exception})"
 
     @classmethod
     def update_user_info(
@@ -44,12 +44,12 @@ class User(SQLModel, table=True):
                     user.name = new_value
                 case "ID" if type(new_value) is int:
                     user.user_id = new_value
-                case "Password" if type(new_value) is str:
+                case "Password" if type(new_value) is dict(str, str):
                     user.user_pass = new_value
                 case _:
                     return False
 
-            session.update(user)
+           
             session.commit()
 
             return True
@@ -63,25 +63,31 @@ class User(SQLModel, table=True):
             user = session.exec(statement).first()
 
             session.delete(user)
+            session.commit()
 
-    @staticmethod
-    def login(engine, user_id_ent, user_pass_ent) -> bool:
+    @classmethod
+    def login(cls, engine, user_id_ent: int, user_pass_ent: str) -> bool:
         with Session(engine) as session:
-            statement = select(User).where(
-                User.user_id == user_id_ent, User.user_pass == user_pass_ent
+
+            print(engine, user_id_ent, user_pass_ent)
+
+            statement = select(cls).where(
+                cls.user_id == user_id_ent, cls.user_pass == user_pass_ent
             )
             user_exists = session.exec(statement).first()
-
+                
             return user_exists is not None
 
+    @staticmethod
+    def db_config():
+        
+        db_file_name = os.path.join(os.path.dirname(__file__), "users.db")
 
-def db_config():
-    db_file_name = "users.db"
+        engine = create_engine(f"sqlite:///{db_file_name}")
+        SQLModel.metadata.create_all(engine)
 
-    engine = create_engine(f"sqlite:///{db_file_name}")
-    SQLModel.metadata.create_all(engine)
-
-    return engine
+        return engine
+        
 
 
 """def home_page(page: ft.Page):
@@ -131,7 +137,7 @@ def db_config():
     page.add(ft.Container(alignment=ft.Alignment.TOP_CENTER, content=welcome_message, expand=True))"""
 
 
-"""def main(page: ft.Page):
+def main(page: ft.Page):
     #Configuring Page 
     page.title = "Login"
 
@@ -142,10 +148,9 @@ def db_config():
 
     page.bgcolor = ft.Colors.TRANSPARENT
 
-
-
     page.decoration = ft.BoxDecoration(image=ft.DecorationImage(src="Login_page_background.jpg", fit=ft.BoxFit.FILL))
 
+    
     #Page Title
 
     login_title = ft.Text("Login", theme_style=ft.TextThemeStyle.DISPLAY_SMALL)
@@ -154,23 +159,29 @@ def db_config():
 
     user_name_icon = ft.Icon(ft.Icons.ACCOUNT_CIRCLE_ROUNDED, color=ft.Colors.PRIMARY, size=40)
 
-    user_name_text_field = ft.TextField(label="User ID", max_length=5, counter="")
+    user_id_text_field = ft.TextField(label="User ID", max_length=5, counter="")
 
-    user_name_field = ft.Row([user_name_icon, user_name_text_field], tight=True)
+    user_name_field = ft.Row([user_name_icon, user_id_text_field], tight=True)
 
     #Password fields
 
     user_password_icon = ft.Icon(ft.Icons.PASSWORD, color=ft.Colors.PRIMARY, size=40)
 
-    user_name_text_field = ft.TextField(label="Password/Pin", password=True, can_reveal_password=True)
+    user_password_text_field = ft.TextField(label="Password/Pin", password=True, can_reveal_password=True)
 
-    user_password_field = ft.Row([user_password_icon, user_name_text_field], tight=True)
+    user_password_field = ft.Row([user_password_icon, user_password_text_field], tight=True)
 
     #Login button
 
-    def test(e): print("test")
+    def login_on_click(): 
+        engine = User.db_config()
 
-    login_button = ft.FilledButton(content=ft.Text("Login"), on_click=test)
+        login_success = User.login(engine, int(user_id_text_field.value), str(user_password_text_field.value))
+
+        print(login_success)
+
+    login_button = ft.FilledButton(content=ft.Text("Login"), on_click=login_on_click)
+
 
     #Login fields 
 
@@ -183,9 +194,9 @@ def db_config():
                      content=ft.Container(padding=10, content=login_fields),
                      opacity=0.65
                     )
-            )"""
+            )
 
 
 if __name__ == "__main__":
-    # ft.run(main)
-    engine = db_config()
+    ft.run(main)
+    
